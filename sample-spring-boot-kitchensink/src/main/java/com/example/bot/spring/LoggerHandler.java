876b4a9806a8f9f6898c7a3f68309629fab34ccd
VaniOFX <iebardarov@gmail.com>
2017-11-20 21:57:14 +0800

    tests

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LoggerHandler.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LoggerHandler.java
@@ -1,6 +1,7 @@
 package com.example.bot.spring;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LoggerHandler implements EventHandler {
 
@@ -8,6 +9,8 @@ public class LoggerHandler implements EventHandler {
 	@Override
 	public String handleEvent(ArrayList<String> inputArray) {
 		database.logQuestion(inputArray.get(1));
+		LineCommunicator lg = new LineCommunicator();
+		lg.pushCustomerNotification(new ArrayList<String>(Arrays.asList("U7284687917ae6c74fdca2ba21f055e78")), "Someone is asking: ");
 		return MessageHandler.DEFAULTANSWER;
 	}
 
